e91a1acdb2e3b0f8c8c85ef782b894241cbd9a94
ngcaden <quangnguyen30795@gmail.com>
2016-10-15 14:55:52 +0100

    Add extraction of longitude and latitdue

--- cyclone-track.py
+++ cyclone-track.py
@@ -9,20 +9,31 @@ with open (filename) as f:
     lines= sum(1 for line in enumerate(f))
 
 #Create an empty list for writing read infomation
-data = []
+raw_data = []
 
 #Open file for comprehension
 file = open(filename, 'r')
 
-
-i = 1
-
 #Reading file
+i = 0
 while i < lines:
+    #Read each line
     temp = file.readline()
-    print temp
-    data.append([temp],)
-    i += 1
     
-print data
+    #Split into individual strings before adding to raw_data
+    raw_data.append(temp.split(','),)
+    i += 1
 
+#Create empty list to store longitude and latitude information
+data = []
+
+#Extract longitude and latitude
+i = 0
+while i < len(raw_data):
+    #Get each line
+    line = raw_data[i]
+    #Get the longitude and latitude
+    data.append([line[6],line[7]])
+    i += 1
+
+print data
