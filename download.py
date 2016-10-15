58a19636527808deee2df173f53c501ba3bcac4d
ngcaden <quangnguyen30795@gmail.com>
2016-10-15 22:48:06 +0100

    add download method and downloaded files

---
+++ download.py
@@ -0,0 +1,35 @@
+import urllib
+import os, os.path
+
+#Starting from year 2009
+year = 2009
+
+
+#Download from 1980 to 2009
+while year > 1979:
+    Year = str(year)
+    
+    #Make new folder with the year included
+    os.makedirs(Year)
+    
+    #cd that folder
+    os.chdir(Year)
+    
+    #Download all the cyclones of that year
+    number = 01
+    while number < 45:
+        
+        Number = str("%02d" % (number,))
+        
+        #Generate URL for download
+        url = "http://www.usno.navy.mil/NOOC/nmfc-ph/RSS/jtwc/best_tracks/" + Year + "/" + Year +"s-bwp/bwp" + Number + Year +".txt"
+        
+        print url
+        
+        #Generate filename
+        filename = Number + Year + '.txt'
+        urllib.urlretrieve (url,filename)
+        number += 1    
+    
+    os.chdir('..')
+    year -= 1
