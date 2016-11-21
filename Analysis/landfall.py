0735fb786a584503f3f8666da54b42d2f0a910f0
Ubuntu <ubuntu@ip-172-31-20-48.eu-west-1.compute.internal>
2016-11-21 22:27:20 +0000

    added computed data for Malaysia

--- Analysis/landfall.py
+++ Analysis/landfall.py
@@ -13,7 +13,7 @@
 length_division = 20.
 # List the countries that are being investigated
 # countries = ['China','Japan','Philippines','South-Korea','Taiwan','Vietnam']
-countries = ['Philippines']
+countries = ['Malaysia']
 # Specify folder name to store output files
 folder_name = 'landfall'
 # Specify cyclone track data file to load
